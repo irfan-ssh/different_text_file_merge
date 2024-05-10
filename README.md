53d3aaf0dfe29013d71a371875ef44d2f4754630
Sheikh Irfan <irfanssh111@gmail.com>
2024-05-11 00:51:36 +0500

    python file update and Readme.md file update

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# different_text_file_merge
- merege different text files into one single file
+# Python File Merger Script
+
+## Introduction
+This Python script merges the contents of text files in a specified directory into a single file.
+
+## Installation
+No installation is required. Simply ensure you have Python installed on your system.
+
+## Usage
+To use the script, run it from the command line and provide the input directory containing the text files to merge and the output directory where the merged file will be saved.
+
+```bash
+python merge_files.py
--- file_merge.py
+++ file_merge.py
@@ -1,16 +1,20 @@
 import os
-directory_path_name = input("Enter the directory path: ")
-def merge_files_in_directory(directory_path, output_path):
+
+def merge_files_in_directory(input_directory, output_directory):
+    output_path = os.path.join(output_directory, "merge.txt")
     with open(output_path, 'w') as output_file:
-        for file_name in os.listdir(directory_path):
-            file_path = os.path.join(directory_path, file_name)
+        for file_name in os.listdir(input_directory):
+            file_path = os.path.join(input_directory, file_name)
             if os.path.isfile(file_path):
                 with open(file_path, 'r') as input_file:
                     links = input_file.readlines()
-                    output_file.writelines(links)
+                    for link in links:  
+                        output_file.write(link.strip() + "\n")  # Space of between every line
+                        output_file.write("\n")
     print("Files merged successfully!")
 
 if __name__ == "__main__":
-    directory_path = directory_path_name
-    output_path = "merge.txt"
-    merge_files_in_directory(directory_path, output_path)
+    input_directory = input("Enter the input Folder path: ")
+    output_directory = input("Enter the output Folder path: ")
+    
+    merge_files_in_directory(input_directory, output_directory)
