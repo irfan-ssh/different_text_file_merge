dfe3b17fdcbf0e404388f6b52f9e9321fdd8f67a
Sheikh Irfan <irfanssh111@gmail.com>
2024-05-11 00:12:05 +0500

    folder path added

--- file_merge.py
+++ file_merge.py
@@ -1,12 +1,16 @@
-def merge_files(file_paths, output_path):
+import os
+directory_path_name = input("Enter the directory path: ")
+def merge_files_in_directory(directory_path, output_path):
     with open(output_path, 'w') as output_file:
-        for file_path in file_paths:
-            with open(file_path, 'r') as input_file:
-                links = input_file.readlines()
-                output_file.writelines(links)
+        for file_name in os.listdir(directory_path):
+            file_path = os.path.join(directory_path, file_name)
+            if os.path.isfile(file_path):
+                with open(file_path, 'r') as input_file:
+                    links = input_file.readlines()
+                    output_file.writelines(links)
+    print("Files merged successfully!")
 
 if __name__ == "__main__":
-    file_paths = ["file.txt", "file1.txt"]
+    directory_path = directory_path_name
     output_path = "merge.txt"
-    merge_files(file_paths, output_path)
-    print("Files merged successfully!")
+    merge_files_in_directory(directory_path, output_path)
